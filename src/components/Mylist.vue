e0c20a306fa8eee369b2b6ca86b45a5fd7db7883
yael <yael@soixantecircuits.fr>
2017-01-12 17:33:24 +0100

    Began pagination

--- src/components/Mylist.vue
+++ src/components/Mylist.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="mylist">
+    <button v-on:click="goToPreviousPage">Prev</button>
+    <button v-on:click="goToNextPage">Next</button> Items per page <input type="number" min="0" v-model="nbItemsToDisplay"
+      v-on:keyup="checkCurrentPageValidity()">
     <table align="center" class="mylist-table">
       <tr class="mylist-line">
+        <th></th>
         <th class="filename-field">Filename</th>
         <th class="path-field">Path</th>
         <th class="state-field">State</th>
       </tr>
-      <tr v-for="(item, index) in items" v-bind:class="[ 'mylist-line', index % 2 == 0 ? 'mylist-line-alt' : '' ]">
+      <tr v-for="(item, index) in items" v-if="index >= (nbItemsToDisplay * currentListPage) && index < ((nbItemsToDisplay * currentListPage) + nbItemsToDisplay)"
+        v-bind:class="[ 'mylist-line', index % 2 == 0 ? 'mylist-line-alt' : '' ]">
+        <td class="delete-field"><button v-on:click="deleteItem(item._id)">Delete</button></td>
         <td class="filename-field">{{ item.filename }}</td>
         <td class="path-field">{{ item.path }}</td>
         <td class="state-field">
@@ -19,6 +25,8 @@
         </td>
       </tr>
     </table>
+    <button v-on:click="goToPreviousPage">Prev</button>
+    <button v-on:click="goToNextPage">Next</button>
   </div>
 </template>
 
@@ -29,7 +37,9 @@
 
   let data = {
     items: [],
-    states: config.states
+    nbItemsToDisplay: 2,
+    currentListPage: 0,
+    states: config.states,
   }
 
   export default {
@@ -38,7 +48,7 @@
     },
     methods: {
       getMediaList: getMediaList,
-      
+
       updateState(id, state) {
         request.put(`http://${config.moderatorServer}/api/v1/medias/${id}`)
           .send({ state: state })
@@ -48,13 +58,43 @@
           return elem._id === id
         })
         item.editing = false
+      },
+
+      deleteItem(id) {
+        request.delete(`http://${config.moderatorServer}/api/v1/medias/${id}`)
+          .end((err, res) => {
+            if (!err) {
+              let itemToDelete = this.items.find((item) => { return item._id === id })
+              let index = this.items.indexOf(itemToDelete)
+              this.items.splice(index, 1)
+            }
+            return err
+          })
+      },
+
+      goToPreviousPage() {
+        this.currentListPage = this.currentListPage > 0 ? this.currentListPage - 1 : 0
+      },
+
+      goToNextPage() {
+        this.currentListPage =
+          this.currentListPage < ((this.items.length / this.nbItemsToDisplay) - 1) ?
+            this.currentListPage + 1 :
+            ((this.items.length / this.nbItemsToDisplay) - 1)
+      },
+
+      checkCurrentPageValidity() {
+        if ((this.currentListPage * this.nbItemsToDisplay) >= this.items.length) {
+          console.log('item length ' + this.items.length + '  displat ' + this.nbItemsToDisplay)
+          this.currentListPage = Math.max(Math.floor(this.items.length / this.nbItemsToDisplay) - 1, 0)
+        }
       }
     }
   }
 
   getMediaList()
   function getMediaList() {
-    request.get(`http://${config.moderatorServer}/api/v1/medias/all`)
+    request.get(`http://${config.moderatorServer}/api/v1/medias/`)
       .end(function (err, res) {
         let list = JSON.parse(res.text)
         list.forEach((item) => {
@@ -92,12 +132,15 @@
   background-color: #d0d0d0;
 }
 
+.delete-field {
+  width: 10%;
+}
 .filename-field {
   width: 30%;
 }
 
 .path-field {
-  width: 50%;
+  width: 40%;
 }
 
 .state-field {
