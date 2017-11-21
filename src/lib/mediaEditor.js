5be1aeda8152c47719bb691eeabc1e844f021f66
Dmitry Martynenko <dmitry@hirebrains.co>
2017-11-21 21:29:16 +0200

    💎 get editable parts

--- src/lib/mediaEditor.js
+++ src/lib/mediaEditor.js
@@ -1,3 +1,80 @@
+var cheerio = require('cheerio')
+
+let editableParts = []
+let currentId = ''
+
+/**
+ * Get an array of producers
+ * @param $
+ * @param $producers
+ * @returns {Array}
+ */
+let extractProducers = ($, $producers) => {
+  let producers = []
+
+  $producers.each((i, el) => {
+    producers.push({
+      id: $(el).attr('id'),
+      title: $(el).attr('title')
+    })
+  })
+
+  return producers
+}
+
+let extractEntries = ($, $playlists, producers) => {
+  let entries = []
+
+  let findProducerById = (producerId) => {
+    for (let i = 0; i < producers.length; i++) {
+      if (producers[i].id === producerId) {
+        return producers[i]
+      }
+    }
+
+    return false
+  }
+
+  $playlists.each((i, playlist) => {
+    if ($(playlist).children().length > 0) {
+      $(playlist).find('entry').each((j, entry) => {
+        let producerId = $(entry).attr('producer')
+        if (producerId) {
+          let producer = findProducerById(producerId)
+          if (false === producer) {
+            return
+          }
+
+          entries.push({
+            in: $(entry).attr('in'),
+            out: $(entry).attr('out'),
+            producer: producer
+          })
+        }
+      })
+    }
+  })
+
+  return entries
+}
+
+/**
+ * Get a list of editable parts from melt XML
+ * @param melt
+ * @returns {Array}
+ */
+let getEditableParts = (melt) => {
+  let $ = cheerio.load(melt, {xmlMode: true})
+  let $playlists = $('playlist#playlist1', 'mlt')
+  let $producers = $('producer', 'mlt')
+
+  if (!$playlists || !$playlists.length || !$producers || !$producers.length) {
+    return []
+  }
+
+  return extractEntries($, $playlists, extractProducers($, $producers))
+}
+
 export default {
   computed: {
     hasEditableParts () {
@@ -12,6 +89,16 @@ export default {
       }
 
       media = this.media
+      if (media.meta && media.meta.etnaInput && media.meta.melt && media.meta.melt.scriptString) {
+        if (currentId !== media._id) {
+          editableParts = getEditableParts(media.meta.melt.scriptString)
+        }
+
+        this.editableParts = editableParts
+        console.log(this.editableParts)
+      }
+
+      currentId = media._id
     }
   },
   data () {
