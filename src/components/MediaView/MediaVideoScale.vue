cb4633da78b9f6b1e608bcc149ca74ca7b403b40
Dmitry Martynenko <dmitry@hirebrains.co>
2017-11-22 18:43:00 +0200

    💎 move fragment methods to mixin

--- src/components/MediaView/MediaVideoScale.vue
+++ src/components/MediaView/MediaVideoScale.vue
@@ -9,11 +9,12 @@
 <script>
   import duration from '../../lib/duration'
   import timeline from '../../lib/timeline'
+  import fragment from '../../lib/fragment'
   import { fabric } from 'fabric'
 
   export default {
     name: 'media-video-scale',
-    mixins: [timeline],
+    mixins: [timeline, fragment],
     props: {
       fragmentIn: {
         type: Number,
@@ -49,49 +50,6 @@
           width: canvasContainer.offsetWidth,
           height: canvasContainer.offsetHeight
         }
-      },
-      getFragmentSizeAndPosition () {
-        const fragmentHeight = 8
-        const fragmentMinWidth = 16
-
-        let size = this.getCanvasSize()
-        let fragmentDuration = this.fragmentOut - this.fragmentIn
-        let width = (fragmentDuration * 100 / this.total) * size.width / 100
-        let left = (this.fragmentIn * 100 / this.total) * size.width / 100
-
-        if (fragmentDuration <= 0) {
-          fragmentDuration = 1
-        }
-
-        return {
-          left: left,
-          top: size.height - fragmentHeight,
-          width: width < fragmentMinWidth ? fragmentMinWidth : width,
-          height: fragmentHeight
-        }
-      },
-      updateFragmentSize () {
-        let fragmentSize = this.getFragmentSizeAndPosition()
-
-        this.fragment.set('left', fragmentSize.left)
-        this.fragment.set('width', fragmentSize.width).setCoords()
-        this.canvas.requestRenderAll()
-      },
-      drawFragment () {
-        let fragmentSize = this.getFragmentSizeAndPosition()
-        this.fragment = new fabric.Rect({
-          fill: '#fd4f4f',
-          left: fragmentSize.left,
-          top: fragmentSize.top,
-          width: fragmentSize.width,
-          height: fragmentSize.height,
-          selectable: true,
-          hasBorders: false,
-          hasControls: false,
-          lockMovementY: true
-        })
-
-        this.canvas.add(this.fragment)
       },
       drawObjects () {
         this.drawFragment()
---
+++ src/lib/fragment.js
@@ -0,0 +1,47 @@
+export default {
+  methods: {
+    getFragmentSizeAndPosition () {
+      const fragmentHeight = 8
+      const fragmentMinWidth = 16
+
+      let size = this.getCanvasSize()
+      let fragmentDuration = this.fragmentOut - this.fragmentIn
+      let width = (fragmentDuration * 100 / this.total) * size.width / 100
+      let left = (this.fragmentIn * 100 / this.total) * size.width / 100
+
+      if (fragmentDuration <= 0) {
+        fragmentDuration = 1
+      }
+
+      return {
+        left: left,
+        top: size.height - fragmentHeight,
+        width: width < fragmentMinWidth ? fragmentMinWidth : width,
+        height: fragmentHeight
+      }
+    },
+    updateFragmentSize () {
+      let fragmentSize = this.getFragmentSizeAndPosition()
+
+      this.fragment.set('left', fragmentSize.left)
+      this.fragment.set('width', fragmentSize.width).setCoords()
+      this.canvas.requestRenderAll()
+    },
+    drawFragment () {
+      let fragmentSize = this.getFragmentSizeAndPosition()
+      this.fragment = new fabric.Rect({
+        fill: '#fd4f4f',
+        left: fragmentSize.left,
+        top: fragmentSize.top,
+        width: fragmentSize.width,
+        height: fragmentSize.height,
+        selectable: true,
+        hasBorders: false,
+        hasControls: false,
+        lockMovementY: true
+      })
+
+      this.canvas.add(this.fragment)
+    }
+  }
+}
